b4fac3bea2ff909e70a96fddbb2e16a82024c54e
Roann Yanes <ryanes@nd.edu>
2017-01-21 14:34:49 -0500

    cards.h

---
+++ cards.h
@@ -0,0 +1,15 @@
+// Authors: Matthew D'Alonzo,Grace Milton, and Roann Yanes
+// Program: cards.h
+// #DEFINE SIZE1 =
+
+class Cards {
+	public:
+		Cards();
+		~Cards();
+		void setSuit(char);
+		void setKind();
+
+	private:
+		char suit[];
+		char kind[];
+		char [][]
