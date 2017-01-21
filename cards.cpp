7a863994824730e9152a0f584f39787c2551d821
Grace Milton <gmilton@nd.edu>
2017-01-21 14:43:26 -0500

    Add cards.cpp

---
+++ cards.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include "cards.h"
+
+Cards::Cards() { }
+
+Cards::~Cards() { }
+
+char Cards::getKind() 
+{ return kind; }
+
+void Cards::setKind(char k) { }
+
+void draw
