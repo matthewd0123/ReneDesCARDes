8b5d1a5f458d86a7cf1c0f229d71e537c80cdfa3
matthewd0123 <mdalonz1@nd.edu>
2017-01-21 14:28:30 -0500

    Added main function and some comments

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+main.cpp
+
+This is the main file that runs the card application
+
+Authors:
+Matthew D'Alonzo
+Grace Milton
+Roann Yanes 
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  cout << "Welcome to the Card Program!" << endl;
+  
+  return 0;
+}
